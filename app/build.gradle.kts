1ac10c70579e8263cf06abf76cb73bad06b34378
RipTin <142385237+justinmcneal@users.noreply.github.com>
2025-08-21 08:07:10 +0800

    prompted

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt)
-    alias(libs.plugins.kotlin.kapt)
+    id("kotlin-kapt")
 }
 
 android {
