f8c43448144bdc2d17dae63b4c7692aa4ad98b4f
RipTin <142385237+justinmcneal@users.noreply.github.com>
2025-08-21 11:12:43 +0800

    moved the base url inside the gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "BASE_URL", "http://192.168.1.104:8000/api/users/")
     }
 
     buildTypes {
